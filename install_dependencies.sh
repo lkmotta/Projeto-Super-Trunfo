c4abbd8f823560fe9845121e0d6dcc8196428aa1
Lucas H. Motta <lucashmotta.contact@gmail.com>
2025-02-01 18:48:53 -0300

    script de instalação

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -12,18 +12,20 @@ if [ "$resposta" = "Y" ] || [ "$resposta" = "y" ] || [ -z "$resposta" ]; then
     # instalando dependências do raylib
     echo "Instalando dependências do Raylib..."
 
-    if [ "$(cat /etc/os-release | grep -oP '(?<=^NAME=).*')" = "Ubuntu" ]; then
+    os_name=$(grep '^NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+    if [ "$os_name" = "Ubuntu" ]; then
         # UBUNTU
-        sudo apt install libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev libxkbcommon-dev
-    elif [ "$(cat /etc/os-release | grep -oP '(?<=^NAME=).*')" = "Fedora" ]; then
+        sudo apt install -y libasound2-dev libx11-dev libxrandr-dev libxi-dev libgl1-mesa-dev libglu1-mesa-dev libxcursor-dev libxinerama-dev libwayland-dev libxkbcommon-dev
+    elif [ "$os_name" = "Fedora" ]; then
         # FEDORA
-        sudo dnf install alsa-lib-devel mesa-libGL-devel libX11-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel libatomic
-    elif [ "$(cat /etc/os-release | grep -oP '(?<=^NAME=).*')" = "Arch Linux" ]; then
+        sudo dnf install -y alsa-lib-devel mesa-libGL-devel libX11-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel libatomic
+    elif [ "$os_name" = "Arch Linux" ]; then
         # ARCH LINUX
-        sudo pacman -S alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama
-    elif [ "$(cat /etc/os-release | grep -oP '(?<=^NAME=).*')" = "Void Linux" ]; then
+        sudo pacman -S --noconfirm alsa-lib mesa libx11 libxrandr libxi libxcursor libxinerama
+    elif [ "$os_name" = "Void Linux" ]; then
         # VOID LINUX
-        sudo xbps-install alsa-lib-devel libglvnd-devel libX11-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel mesa MesaLib-devel
+        sudo xbps-install -y alsa-lib-devel libglvnd-devel libX11-devel libXrandr-devel libXi-devel libXcursor-devel libXinerama-devel mesa MesaLib-devel
     else
         echo "Sistema operacional não suportado. Abortando a instalação."
         exit 1
@@ -39,7 +41,7 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/raylib/lib
 # verificando se tudo foi configurado corretamente
 if gcc --version >/dev/null 2>&1 && make --version >/dev/null 2>&1 && [ -d "$(pwd)/raylib/lib" ]; then
     echo "Configuração concluída com sucesso!"
-    echo "AVISO: Antes de compilar, não se esqueça do comando `make clean` para limpar os arquivos antigos."
+    echo "AVISO: Antes de compilar, não se esqueça do comando make clean para limpar os arquivos antigos."
 else
     echo "Erro durante a configuração. Verifique as dependências manualmente, ou clone o repositório novamente."
 fi
